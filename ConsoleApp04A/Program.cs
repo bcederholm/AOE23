f6ec7daa70c9c8b977ee720676df2f0ab299f97b
benjamin-cederholm <benjamin.cederholm@bokadirekt.se>
2023-12-10 23:15:49 +0100

    A04A inspected

--- ConsoleApp04A/Program.cs
+++ ConsoleApp04A/Program.cs
@@ -1,30 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿/*
+ * FileName: Program.cs
+ * Author: Benjamin Cederholm
+ * Date Created: 2023-10-04
+ * Last Modified: 2023-12-10
+ * Description: https://adventofcode.com/2023/day/4 - Part One
+ * Lessons learned: Linq
+ */
 
-Console.WriteLine("Hello, World!");
+const string filePath = "input.txt";
+var lines = File.ReadAllLines(filePath);
 
-string filePath = "C:\\repos\\offside\\ConsoleApp01\\ConsoleApp04A\\Input4A.txt";
-string[] lines = File.ReadAllLines(filePath);
+var splitters = new[] { '|', ':' };
+var totalSum = (from line in lines select line.Replace("  ", " ") 
+    into line2 select line2.Replace("  ", " ")
+    into line2 select line2.Replace("Card ", "")
+    into line2 select line2.Split(splitters, StringSplitOptions.RemoveEmptyEntries)
+    into sections let winningNumbers = sections[1].Trim().Split(' ')
+    let myNumbers = sections[2].Trim().Split(' ')
+    select myNumbers.Intersect(winningNumbers).ToArray()
+    into matches where matches.Length > 0 select Math.Pow(2, matches.Length - 1)
+    into points select (int)points).Sum();
 
-char[] splitters = new char[] { '|', ':' };
-
-var totalSum = 0;
-
-foreach (var line in lines)
-{
-    var line2 = line.Replace("  ", " ");
-    line2 = line2.Replace("  ", " ");
-    line2 = line2.Replace("Card ", "");
-    
-    var sections = line2.Split(splitters, StringSplitOptions.RemoveEmptyEntries);
-    var winningNumbers = sections[1].Trim().Split(' ');
-    var myNumbers = sections[2].Trim().Split(' ');
-    var matches = myNumbers.Intersect(winningNumbers).ToArray();
-    
-    if (matches.Length > 0)
-    {
-        var points = Math.Pow(2, matches.Length -1);
-        totalSum += (int)(points);
-    }
-}
-
-Console.WriteLine(totalSum);
+Console.WriteLine($"Answer {totalSum}");
