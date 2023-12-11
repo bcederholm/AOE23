fa0551d587a6afa3f7dddd0ce4ac2407e4eeb4cc
benjamin-cederholm <benjamin.cederholm@bokadirekt.se>
2023-12-11 13:05:18 +0100

    Inspect 09A

--- ConsoleApp09A/Program.cs
+++ ConsoleApp09A/Program.cs
@@ -1,22 +1,27 @@
-﻿string filePath1 = "C:\\repos\\offside\\ConsoleApp01\\ConsoleApp09A\\Input9.txt";
-string[] lines = File.ReadAllLines(filePath1);
+﻿/*
+ * FileName: Program.cs
+ * Author: Benjamin Cederholm
+ * Date Created: 2023-10-09
+ * Last Modified: 2023-12-11
+ * Description: https://adventofcode.com/2023/day/9 - Part One
+ * Keywords: N/A
+ */
 
+const string filePath1 = "input.txt";
+var lines = File.ReadAllLines(filePath1);
 
 var grandTotalExtrapolateNumbers = 0;
 foreach (var line in lines)
 {
-    var splittedNumbers = line.Split(' ');
-    
+    var splitNumbers = line.Split(' ');
     var rowsOfNumbers = new List<List<int>>();
-    var numbers = new List<int>();
     var finished = false;
-
-    var currentNumbers = splittedNumbers.Select(c => int.Parse(c)).ToList();;
+    var currentNumbers = splitNumbers.Select(int.Parse).ToList();
     rowsOfNumbers.Add(currentNumbers);
     
     while (!finished)
     {
-        numbers = new List<int>();
+        var numbers = new List<int>();
         for (var i = 0; i < currentNumbers.Count - 1; i++)
         {
             numbers.Add(currentNumbers[i + 1] - currentNumbers[i]);
@@ -24,26 +29,21 @@ foreach (var line in lines)
         currentNumbers = numbers;
         rowsOfNumbers.Add(currentNumbers);
 
-        if (!numbers.Any(n => n != 0))
+        if (numbers.All(n => n == 0))
         {
             finished = true;
         }
     }
 
-    var sum = 0;
     var currentRowExtrapolateNumber = 0;
     for (var i = rowsOfNumbers.Count - 1; i > 0; i--)
     {
-        var currentRow = rowsOfNumbers[i];
         var aboveRow = rowsOfNumbers[i-1];
-        
         var aboveRowLastNumber = aboveRow.Last();
         
         currentRowExtrapolateNumber = aboveRowLastNumber + currentRowExtrapolateNumber;
-        
-        Console.WriteLine($"difference: {currentRowExtrapolateNumber} ");
     }
     grandTotalExtrapolateNumbers += currentRowExtrapolateNumber;
 }
 
-Console.WriteLine($"Grand total: {grandTotalExtrapolateNumbers}");
+Console.WriteLine($"Answer: {grandTotalExtrapolateNumbers}");
